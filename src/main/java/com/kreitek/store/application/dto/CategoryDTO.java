9284d941cecdb1cda39c8cfc05872564a5059ec4
CMerino17 <carlos.merinocrespo@gmail.com>
2023-05-12 13:04:16 +0200

    commit inicial

---
+++ src/main/java/com/kreitek/store/application/dto/CategoryDTO.java
@@ -0,0 +1,10 @@
+package com.kreitek.store.application.dto;
+
+import java.io.Serializable;
+
+public class CategoryDTO implements Serializable {
+    private Long id;
+    private String name;
+    private String description;
+    private byte[] image;
+}
